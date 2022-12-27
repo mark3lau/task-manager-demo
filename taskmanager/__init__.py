5a6da7c11612f7d7b3e0714e3ba6637c5a560fc1
mark3lau <mark3lau@gmail.com>
2022-12-27 09:35:51 +0000

    deploy

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,6 +7,8 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+db = SQLAlchemy(app)
+
 
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
@@ -15,8 +17,6 @@ else:
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
-db = SQLAlchemy(app)
 
 
 from taskmanager import routes #noqa
